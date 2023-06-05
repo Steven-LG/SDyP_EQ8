628cd2596447c82f08ff457685f712f80a09f5a3
Steven_LG <100967107+Steven-LG@users.noreply.github.com>
2023-06-04 22:16:57 -0600

    C-1

---
+++ src/main/java/ClientTest.java
@@ -0,0 +1,43 @@
+package org.example;
+
+import shared.HostSpecs;
+
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+import java.net.Socket;
+import java.util.concurrent.atomic.AtomicReference;
+
+public class ClientTest {
+
+    public static boolean isServer = true;
+    public static void main(String[] args) {
+        System.out.println("Running as client");
+        Thread threadTask = new Thread(() -> {
+            //String mostUsableHost = Collections.max(hostsInfo.entrySet(), Map.Entry.comparingByValue()).getKey();
+            //String mostUsableHost = "localhost";
+
+            try {
+                Socket cSocket = new Socket("25.8.91.48", 5555);
+                AtomicReference<ObjectOutputStream> objOutputStream = new AtomicReference<>(new ObjectOutputStream(cSocket.getOutputStream()));
+
+                while(!isServer){
+                    HostSpecs clientMessage = new HostSpecs();
+                    clientMessage.getCurrentUsage();
+
+                    objOutputStream.get().writeObject(clientMessage);
+                    System.out.println("Object sent");
+                    objOutputStream.get().flush();
+                    Thread.sleep(2000);
+                }
+
+            } catch (IOException e) {
+                System.out.println(e);
+                throw new RuntimeException(e);
+            } catch (InterruptedException e) {
+                System.out.println(e);
+                throw new RuntimeException(e);
+            }
+        });
+        threadTask.start();
+    }
+}
